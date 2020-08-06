9eebc94ac99209caf47cf52ab10ae0fb37c67317
TomiisinA <33558873+TomiisinA@users.noreply.github.com>
2020-08-06 16:34:33 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,14 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Name = 'Oluwatomisin Ajayi'
+>>> Email = 'tomisinajayi@gmail.com'
+>>> Language ='python'
+>>> biostack = 'software development'
+>>> slack username = '@oluwatomisin'
+SyntaxError: invalid syntax
+>>> slackusername = '@oluwatomisin'
+>>> print(Name,Email,Language,biostack)
+Oluwatomisin Ajayi tomisinajayi@gmail.com python software development
+>>> print(slackusername)
+@oluwatomisin
+>>> 
